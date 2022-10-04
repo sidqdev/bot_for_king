feca439294c9c885e0dce576e5eb8947b9e599c7
sidq-corp <dimax258223@gmail.com>
2022-10-04 15:41:09 +0300

    admin

--- admin/admin_panel/admin.py
+++ admin/admin_panel/admin.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from .models import *
 
 
+class TextAdmin(admin.ModelAdmin):
+    list_display = ('id', 'text')
+
 admin.site.register(User)
-admin.site.register(Text)
+admin.site.register(Text, TextAdmin)
 
 # Register your models here.
